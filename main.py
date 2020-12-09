691d443510d2e43639af464f73caead4e9ad79a3
m4dn355 <xm4dn355x@gmail.com>
2020-12-09 19:28:48 +0500

    added Docstrings and Type Hinting

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import sqlite3
-import threading
+
 from time import sleep
 
 from telegram import Bot
@@ -15,18 +14,12 @@ from news_parser import get_current_news, parse_news
 URL = 'https://yandex.ru/news/region/salekhard'
 
 
-conn = sqlite3.connect('news_list.db')
-cursor = conn.cursor()
-# cursor.execute('''CREATE TABLE news (title text, url text, source text, status bool)''')
-# conn.commit()
-
-
 request = Request(connect_timeout=3)
 bot = Bot(request=request, token=API_TOKEN)
 
 
 def log_error(f):
-    """Отлавливание ошибок"""
+    """Отлавливание ошибок телоеграм-бота"""
     def inner(*args, **kwargs):
         try:
             return f(*args, **kwargs)
@@ -41,17 +34,20 @@ def log_error(f):
 
 
 @log_error
-def post_new(post):
-    insert_data(conn, cursor, post['title'], post['url'], post['source'])
+def post_new(post: dict):
+    """Принимает тело поста и постит его в телеграм канал"""
+    insert_data(post['title'], post['url'], post['source'])
     bot.send_message(chat_id=CHANNEL_ID, text=generate_message_text(post), parse_mode='html')
-    update_data(conn, cursor, post['url'])
+    update_data(post['url'])
 
 
-def generate_message_text(post):
+def generate_message_text(post: dict) -> str:
+    """Принимает тело поста и генерирует на его основе текст поста"""
     return f'<a href="{post["url"]}">{post["title"]}</a>\n\n#{post["source"]}'
 
 
 def update_channel_loop():
+    """Бесконечный цикл публикующий новости в телеграм канале"""
     while True:
         current_news = get_current_news(parse_news(URL))
         for post in current_news:
@@ -63,4 +59,3 @@ def update_channel_loop():
 if __name__ == '__main__':
     print('main')
     update_channel_loop()
-    conn.close()
